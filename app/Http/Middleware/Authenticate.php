e1b2ddacc509dafdf5ed38067bda79cf5559f528
Syafiq <syafiq.rezpector@gmail.com>
2018-12-08 20:48:25 +0700

    Fix Authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,21 +2,69 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Contracts\Auth\Factory as Auth;
+
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * The authentication guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory $auth
+     * @return void
+     */
+    public function __construct(Auth $auth)
+    {
+        parent::__construct($auth);
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request $request
-     * @return string
+     * @return mixed
      */
     protected function redirectTo($request)
     {
         if (!$request->expectsJson())
         {
             return route('login');
+        }
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     * @return mixed
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ($request->expectsJson())
+        {
+            if ($this->auth->guard($guard)->guest())
+            {
+                return response('Unauthorized.', 401);
+            }
+
+            return $next($request);
+        }
+        else
+        {
+            return parent::handle($request, $next, $guard);
         }
     }
 }
