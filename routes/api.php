9f4c9eada9d3c8f4532418bb813682327e1703ae
Syafiq <syafiq.rezpector@gmail.com>
2018-12-08 21:02:14 +0700

    Add api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +11,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/** @var \Illuminate\Routing\Router $router */
+$router->group(['namespace' => 'Student', 'prefix' => '/student'], function () use ($router) {
+    $router->group(['middleware' => 'registered.role', 'prefix' => '/auth'], function () use ($router) {
+        $router->post('/refresh', ['middleware' => ['c.jwt.refresh'], 'uses' => 'Auth@postRefresh', 'as' => 'student.auth.refresh.post']);
+        $router->group(['middleware' => 'guest'], function () use ($router) {
+            $router->post('/login', ['uses' => 'Auth@postLogin', 'as' => 'student.auth.login.post']);
+            $router->post('/register', ['uses' => 'Auth@registerStore', 'as' => 'student.auth.register.post']);
+            $router->post('/lost', ['uses' => 'Auth@postLost', 'as' => 'student.auth.lost.post']);
+            $router->patch('/recover', ['middleware' => ['valid.auth.recovery'], 'uses' => 'Auth@patchRecover', 'as' => 'student.auth.recover.patch']);
+        });
+    });
+    $router->group(['middleware' => 'c.jwt.auth.student'], function () use ($router) {
+        $router->get('/dashboard', ['uses' => 'Dashboard@index', 'as' => 'student.dashboard.get']);
+        $router->post('/auth/logout', ['uses' => 'Auth@postLogout', 'as' => 'student.auth.logout.post']);
+        $router->post('/auth/ping', ['uses' => 'Auth@ping', 'as' => 'student.auth.ping.post']);
+        $router->get('/profile', ['uses' => 'Profile@getUpdate', 'as' => 'student.profile.get']);
+        $router->patch('/profile', ['uses' => 'Profile@update', 'as' => 'student.profile.update.patch']);
+        $router->group(['middleware' => 'valid.student.profile'], function () use ($router) {
+            $router->post('/course/create', ['middleware' => 'course.open.unavailable', 'uses' => 'Course@create', 'as' => 'student.course.create.post']);
+            $router->get('/course/start/{question:\d+}', ['middleware' => ['course.open.available', 'valid.question'], 'uses' => 'Course@startEdit', 'as' => 'student.course.start.get']);
+            $router->get('/course/navigation/{question:\d+}', ['middleware' => ['course.open.available', 'valid.question'], 'uses' => 'Course@navigation', 'as' => 'student.course.navigation.get']);
+            $router->patch('/course/start/{question:\d+}', ['middleware' => ['course.open.available', 'valid.question', 'valid.answer'], 'uses' => 'Course@startUpdate', 'as' => 'student.course.start.patch']);
+            $router->post('/course/submit', ['middleware' => ['course.open.available', 'course.open.finished'], 'uses' => 'Course@submit', 'as' => 'student.course.finish.post']);
+            $router->get('/course/result', ['middleware' => 'valid.self.report.detail', 'uses' => 'Course@result', 'as' => 'student.course.result.get']);
+        });
+    });
 });
