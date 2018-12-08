d4338d188fdf37e78c22331478654d58752aa40a
Syafiq <syafiq.rezpector@gmail.com>
2018-12-08 10:42:17 +0700

    Fix seeder

--- database/seeds/AnswerResultsSeeder.php
+++ database/seeds/AnswerResultsSeeder.php
@@ -20,30 +20,31 @@ class AnswerResultsSeeder extends RollbackAbleSeeder
 
     public function run()
     {
+        $answers    = [
+            '.1' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa',
+            '.2' => '70ec998c-064d-4925-b46a-cf4f70fef087',
+        ];
         $categories = [
             '.1' => 'ecb05035-a198-4a6c-a082-32e59331f0f1',
             '.2' => '2a0c84a2-439c-4bc7-9392-3cc9ab14bb18',
             '.3' => '0e9dab20-2e72-4575-a64c-9460a9cc1c3f',
             '.4' => 'b24f1373-f7d5-4307-bc3c-11c788962879',
             '.5' => '11a2c711-56aa-4738-b65e-20adafd3560c',
-            '.6' => '8bc74ee6-68cc-449a-98a9-cf888c4254f4',
         ];
         $data       = [
             [
-                ['id' => '6e851862-9870-4185-99ff-dc94e0ae74f4', 'answer_code' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa', 'category' => 1, 'result' => 10.0],
-                ['id' => '50b73699-6a92-4a15-abc0-2101332da221', 'answer_code' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa', 'category' => 2, 'result' => 20.0],
-                ['id' => '48cad2ee-470b-46d7-a7d8-7d5f2951d817', 'answer_code' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa', 'category' => 3, 'result' => 30.0],
-                ['id' => 'b9217f4e-3006-41d6-ac70-902b3347ab72', 'answer_code' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa', 'category' => 4, 'result' => 40.0],
-                ['id' => '4becffa3-f604-4512-a866-c090ebd0bc12', 'answer_code' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa', 'category' => 5, 'result' => 50.0],
-                ['id' => '927428fb-d8e2-46c1-9c25-db188d0c5b4c', 'answer_code' => '9e5669fe-b0e7-4d8e-a9bf-436b6046c9fa', 'category' => 6, 'result' => 60.0],
+                ['id' => '6e851862-9870-4185-99ff-dc94e0ae74f4', 'answer_code' => 1, 'category' => 1, 'result' => 10.0],
+                ['id' => '50b73699-6a92-4a15-abc0-2101332da221', 'answer_code' => 1, 'category' => 2, 'result' => 20.0],
+                ['id' => '48cad2ee-470b-46d7-a7d8-7d5f2951d817', 'answer_code' => 1, 'category' => 3, 'result' => 30.0],
+                ['id' => 'b9217f4e-3006-41d6-ac70-902b3347ab72', 'answer_code' => 1, 'category' => 4, 'result' => 40.0],
+                ['id' => '4becffa3-f604-4512-a866-c090ebd0bc12', 'answer_code' => 1, 'category' => 5, 'result' => 50.0],
             ],
             [
-                ['id' => '781995d4-98c6-4fd4-be72-2e57879db5df', 'answer_code' => '70ec998c-064d-4925-b46a-cf4f70fef087', 'category' => 1, 'result' => 0.0],
-                ['id' => 'ed90e280-478b-4e61-9a06-26bd6e4431f4', 'answer_code' => '70ec998c-064d-4925-b46a-cf4f70fef087', 'category' => 2, 'result' => 0.0],
-                ['id' => '37ce887e-801f-4da8-b3b9-0c932d0302d8', 'answer_code' => '70ec998c-064d-4925-b46a-cf4f70fef087', 'category' => 3, 'result' => 0.0],
-                ['id' => '476f32c6-aab0-40d1-a735-2d72a1ac7665', 'answer_code' => '70ec998c-064d-4925-b46a-cf4f70fef087', 'category' => 4, 'result' => 0.0],
-                ['id' => '114a4164-62d9-4dc4-be65-b6efc2b2f42c', 'answer_code' => '70ec998c-064d-4925-b46a-cf4f70fef087', 'category' => 5, 'result' => 0.0],
-                ['id' => 'b28d6ea8-bd7c-479c-88ba-3aecba56d270', 'answer_code' => '70ec998c-064d-4925-b46a-cf4f70fef087', 'category' => 6, 'result' => 0.0],
+                ['id' => '781995d4-98c6-4fd4-be72-2e57879db5df', 'answer_code' => 2, 'category' => 1, 'result' => 0.0],
+                ['id' => 'ed90e280-478b-4e61-9a06-26bd6e4431f4', 'answer_code' => 2, 'category' => 2, 'result' => 0.0],
+                ['id' => '37ce887e-801f-4da8-b3b9-0c932d0302d8', 'answer_code' => 2, 'category' => 3, 'result' => 0.0],
+                ['id' => '476f32c6-aab0-40d1-a735-2d72a1ac7665', 'answer_code' => 2, 'category' => 4, 'result' => 0.0],
+                ['id' => '114a4164-62d9-4dc4-be65-b6efc2b2f42c', 'answer_code' => 2, 'category' => 5, 'result' => 0.0],
             ],
         ];
 
@@ -55,7 +56,8 @@ class AnswerResultsSeeder extends RollbackAbleSeeder
             {
                 if (!$model->where('answer_code', '=', $c2['answer_code'])->where('category', '=', $c2['category'])->first())
                 {
-                    $c2['category'] = $categories[".{$c2['category']}"];
+                    $c2['answer_code'] = $answers[".{$c2['answer_code']}"];
+                    $c2['category']    = $categories[".{$c2['category']}"];
                     $model->insert($c2);
                 }
             }
--- database/seeds/QuestionCategoriesSeeder.php
+++ database/seeds/QuestionCategoriesSeeder.php
@@ -14,12 +14,11 @@ class QuestionCategoriesSeeder extends RollbackAbleSeeder
     public function run()
     {
         $data = [
-            ['id' => 'ecb05035-a198-4a6c-a082-32e59331f0f1', 'order' => 1, 'name' => 'Kategori 1', 'description' => 'Menentukan pencapaian target akademik'],
-            ['id' => '2a0c84a2-439c-4bc7-9392-3cc9ab14bb18', 'order' => 2, 'name' => 'Kategori 2', 'description' => 'Menentukan tujuan akademik secara objektif'],
-            ['id' => '0e9dab20-2e72-4575-a64c-9460a9cc1c3f', 'order' => 3, 'name' => 'Kategori 3', 'description' => 'Menentukan metode / strategi pencapaian tujuan akademik'],
-            ['id' => 'b24f1373-f7d5-4307-bc3c-11c788962879', 'order' => 4, 'name' => 'Kategori 4', 'description' => 'Mengelola waktu pelaksanaan akademik'],
-            ['id' => '11a2c711-56aa-4738-b65e-20adafd3560c', 'order' => 5, 'name' => 'Kategori 5', 'description' => 'Mencari sumber yang dibutuhkan untuk mendukung pencapaian tujuan akademik'],
-            ['id' => '8bc74ee6-68cc-449a-98a9-cf888c4254f4', 'order' => 6, 'name' => 'Kategori 6', 'description' => 'Melaksanakan tanggung jawab akademik']
+            ['id' => 'ecb05035-a198-4a6c-a082-32e59331f0f1', 'order' => 1, 'name' => 'Kategori 1', 'description' => 'Nilai Kejujuran'],
+            ['id' => '2a0c84a2-439c-4bc7-9392-3cc9ab14bb18', 'order' => 2, 'name' => 'Kategori 2', 'description' => 'Nilai ikhlas'],
+            ['id' => '0e9dab20-2e72-4575-a64c-9460a9cc1c3f', 'order' => 3, 'name' => 'Kategori 3', 'description' => 'Nilai ketulusan'],
+            ['id' => 'b24f1373-f7d5-4307-bc3c-11c788962879', 'order' => 4, 'name' => 'Kategori 4', 'description' => 'Keyakinan'],
+            ['id' => '11a2c711-56aa-4738-b65e-20adafd3560c', 'order' => 5, 'name' => 'Kategori 5', 'description' => 'Pengamalan'],
         ];
 
         /** @var \Illuminate\Database\Query\Builder $model */
