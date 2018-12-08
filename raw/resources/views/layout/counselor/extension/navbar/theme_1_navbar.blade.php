ca1d68350b0b916f03b91d2b50787cc34422aa6f
Syafiq <syafiq.rezpector@gmail.com>
2018-12-08 16:59:43 +0700

    Change to T

--- raw/resources/views/layout/counselor/extension/navbar/theme_1_navbar.blade.php
+++ raw/resources/views/layout/counselor/extension/navbar/theme_1_navbar.blade.php
@@ -2,7 +2,7 @@
     <!-- Logo -->
     <a href="{{route('counselor.home.dashboard')}}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>F - A</b></span>
+        <span class="logo-mini"><b>T</b></span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>Truthfulness</b></span>
     </a>
