a5ea139b60c9e02487122395f681d3a8e5682d31
Syafiq <syafiq.rezpector@gmail.com>
2018-12-08 20:25:19 +0700

    remove role

--- app/Http/Middleware/ValidAuthRecovery.php
+++ app/Http/Middleware/ValidAuthRecovery.php
@@ -15,12 +15,10 @@ class ValidAuthRecovery
      */
     public function handle($request, Closure $next)
     {
-        $role       = $request->segment(1, null);
         $credential = $request->input('credential', null);
         $token      = $request->post('token', null);
         $user       = User::where('credential', '=', $credential)
             ->where('lost_password', '=', $token)
-            ->where('role', '=', $role)
             ->first();
         if ($user === null)
         {
