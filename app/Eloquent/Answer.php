125e8cc771abbdf03bb2c66678ea400a80bde948
Syafiq <syafiq.rezpector@gmail.com>
2018-12-09 16:26:12 +0700

    Fix category name

--- app/Eloquent/Answer.php
+++ app/Eloquent/Answer.php
@@ -54,21 +54,21 @@ class Answer extends Model
     {
         $analytics = [];
 
-        $analytics['1']['rendah']['guard']          = ['min' => -0.1, 'max' => 33.3];
-        $analytics['1']['rendah']['interval']       = '0 - 33%';
+        $analytics['1']['rendah']['guard']          = ['min' => -0.1, 'max' => 50];
+        $analytics['1']['rendah']['interval']       = '0 - 50%';
         $analytics['1']['rendah']['class']          = 'Rendah';
-        $analytics['1']['rendah']['recommendation'] = 'Tingkat kejujuran <strong>$$0</strong> masih kurang, perlu diperbaiki dan ditingkatkan dengan melakukan tindakan yang mendukung.';
-        $analytics['1']['rendah']['description']    = ['key' => 'Siswa masih kurang memiliki keterampilan dalam  : ', 'value' => '(1) Menentukan pencapaian target akademik (2) menentukan tujuan akademik secara objektif (3) menentukan metode/strategi pencapaian tujuan akademik (4) mengelola waktu pelaksanaan akademik (5) mencari sumber yang dibutuhkan untuk mendukung pencapaian tujuan akademik (6) melaksaanakan tanggung jawab akademik'];
-        $analytics['1']['sedang']['guard']          = ['min' => 33.3, 'max' => 66.6];
-        $analytics['1']['sedang']['interval']       = '34 - 66%';
+        $analytics['1']['rendah']['recommendation'] = 'Nilai Moral <i>Truthfulness</i> <strong>$$0</strong> berda dalam kategori rendah. Dianjurkan untuk melakukan tindakan-tindakan untuk meningkatkan nilai moral <i>Truthfulness</i> anda. Hasil interpretasi tidak mutlak. Anda dapat mengunjungi guru BK apabila ada hal-hal yang tidak dipahami atau hal-hal yang ingin di konsultasikan';
+        $analytics['1']['rendah']['description']    = ['key' => 'saat ini berada dalam kondisi kurang: ', 'value' => '(1) memiliki nilai kebenaran; (2) menerapkan nilai kejujuran; (3) menerapkan nilai yang diyakini dalam diri'];
+        $analytics['1']['sedang']['guard']          = ['min' => 50, 'max' => 75];
+        $analytics['1']['sedang']['interval']       = '50.01 - 75%';
         $analytics['1']['sedang']['class']          = 'Sedang';
-        $analytics['1']['sedang']['recommendation'] = 'Tingkat kejujuran <strong>$$0</strong> sudah cukup, perlu ditingkatkan dengan melakukan tindakan yang mendukung.';
-        $analytics['1']['sedang']['description']    = ['key' => 'Siswa sudah memiliki keterampilan yang cukup dalam : ', 'value' => '(1) Menentukan pencapaian target akademik (2) menentukan tujuan akademik secara objektif (3) menentukan metode/ strategi pencapaian tujuan akademik (4) mengelola waktu pelaksanaan akademik (5) mencari sumber yang dibutuhkan untuk mendukung pencapaian tujuan akademik (6) melaksaanakan tanggung jawab akademik'];
-        $analytics['1']['tinggi']['guard']          = ['min' => 66.6, 'max' => 100.1];
-        $analytics['1']['tinggi']['interval']       = '67 - 100%';
+        $analytics['1']['sedang']['recommendation'] = 'Nilai Moral <i>Truthfulness</i> <strong>$$0</strong> berda dalam kategori sedang. Sehingga perlu melakukan peningkatan nilai moral <i>Truthfulness</i> anda. Hasil interpretasi tidak mutlak. Anda dapat mengunjungi guru BK apabila ada hal-hal yang tidak dipahami atau hal-hal yang ingin di konsultasikan';
+        $analytics['1']['sedang']['description']    = ['key' => 'saat ini berada dalam kondisi sudah belum sepenuhnya: ', 'value' => '(1) memiliki nilai kebenaran; (2) menerapkan nilai kejujuran; (3) menerapkan nilai yang diyakini dalam diri'];
+        $analytics['1']['tinggi']['guard']          = ['min' => 75, 'max' => 100.1];
+        $analytics['1']['tinggi']['interval']       = '75.01 - 100%';
         $analytics['1']['tinggi']['class']          = 'Tinggi';
-        $analytics['1']['tinggi']['recommendation'] = 'Tingkat kejujuran <strong>$$0</strong> sudah tinggi, perlu dipertahankan dan ditingkatkan dengan melakukan tindakan yang mendukung.';
-        $analytics['1']['tinggi']['description']    = ['key' => 'Siswa sudah memiliki keterampilan yang baik untuk : ', 'value' => '(1) Menentukan pencapaian target akademik (2) menentukan tujuan akademik secara objektif (3) menentukan metode/ strategi pencapaian tujuan akademik (4) mengelola waktu pelaksanaan akademik (5) mencari sumber yang dibutuhkan untuk mendukung pencapaian tujuan akademik (6) melaksaanakan tanggung jawab akademik'];
+        $analytics['1']['tinggi']['recommendation'] = 'Nilai Moral <i>Truthfulness</i> <strong>$$0</strong> berada dalam kategori tinggi. Sehingga perlu mempertahankannya dan melakukan tindakan yang mendukung akan hal itu. Hasil interpretasi tidak mutlak. Anda dapat mengunjungi guru BK apabila ada hal-hal yang tidak dipahami atau hal-hal yang ingin di konsultasikan';
+        $analytics['1']['tinggi']['description']    = ['key' => 'saat ini berada dalam kondisi sudah: ', 'value' => '(1) memiliki nilai kebenaran; (2) menerapkan nilai kejujuran; (3) menerapkan nilai yang diyakini dalam diri'];
 
         return $analytics;
     }
