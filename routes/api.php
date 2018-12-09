be0523fdfed404b9539e90760638b02c5256b427
Syafiq <syafiq.rezpector@gmail.com>
2018-12-09 07:34:16 +0700

    Add report publish

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ $router->group(['namespace' => 'Student', 'prefix' => '/student'], function () u
             $router->patch('/course/start/{question}', ['middleware' => ['course.open.available', 'valid.question', 'valid.answer'], 'uses' => 'Course@startUpdate', 'as' => 'student.course.start.patch'])->where('question', '^[1-9][0-9]*');;
             $router->post('/course/submit', ['middleware' => ['course.open.available', 'course.open.finished'], 'uses' => 'Course@submit', 'as' => 'student.course.finish.post']);
             $router->get('/course/result', ['middleware' => 'valid.self.report.detail', 'uses' => 'Course@result', 'as' => 'student.course.result.get']);
+            $router->get('/course/{answer}/detail', ['middleware' => 'valid.self.report.publish', 'uses' => 'Course@detail', 'as' => 'student.course.detail']);
         });
     });
 });
